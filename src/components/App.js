f2e82c74c65de795d7190a6114cbe91ba58a1d17
draktate <draktate@gmail.com>
2022-09-21 17:12:25 +0530

    bug fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,99 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 class Timer extends React.Component {
+
+  
+
   constructor(props) {
+    
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+    
+    this.state = { time: 0, x: 0, y: 0 , display:false, incrMovement:1};
+
+    this.handleClick = this.handleClick.bind(this);
+    this.myFunction = this.myFunction.bind(this);
+
+
   }
+  
   componentDidMount() {
+    console.log("this will get executed only once.")
     
   }
 
   componentWillUnmount() {
+    clearInterval();
+    console.log("this will get executed at end.");
     
   }
 
+  handleClick(e) {
+    this.setState({display:true});
+    let timeInterval = setInterval(()=>{ this.setState({time:this.state.time+1}); }, 1000);
+    document.addEventListener("keydown", this.myFunction);
+  };
 
+  myFunction(event) 
+  {
+    let incrMovement=this.state.incrMovement;
+    let xLeft=this.state.x;
+    let xTop=this.state.y;
+    let key="";    
+    if(this.state.display)
+    {
+      if(event.keyCode==37)
+      {
+        key="left key";
+        xLeft=xLeft-incrMovement;
+      }
+      else if(event.keyCode==38)
+      {
+        key="up key";
+        xTop=xTop-incrMovement;
+      }
+      else if(event.keyCode==39)
+      {
+        key="right key";
+        xLeft=xLeft+incrMovement;
+      }
+      else if(event.keyCode==40)
+      {
+        key="down key";
+        xTop=xTop+incrMovement;
+      }
+      if(xTop<0) xTop=0;
+      if(xLeft<0) xLeft=0;
+          
+      //setBallPosition({left:xLeft , top:xTop});
+      //console.log("x:",xLeft, "y:", xTop );
+      
 
-  render() {
-    return (
- <>
-</>
-    );
+      //document.getElementById("Ball").style.top = xTop;
+      //document.getElementById("Ball").style.left =xLeft;
+      this.setState({x:xLeft, y:xTop});
+      if(xLeft==250 && xTop==250)
+      {
+        this.setState({x:0, y:0});
+        this.setState({time:0});
+        this.setState({display:false}); 
+        clearInterval();
+      }
+    }
   }
+
+
+  render(){
+    return(
+      <>
+      {!this.state.display && <button className='ballProvider' onClick={this.handleClick} > Start </button>}
+      {this.state.display && <div className='hole'> </div>}
+      {this.state.display && <div id="Ball" className="ball" style={{  left : this.state.x, top: this.state.y }}  >   </div> }
+      {this.state.display && <div id="Timer" className="timer">  Time:{this.state.time}  </div> }  
+
+     </>);
+  }
+
 }
 
 export default Timer;
+
